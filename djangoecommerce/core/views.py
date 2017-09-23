084f3a2b1719c6f9e813cc02bfd76794a6194e1f
Luis Paulo Correa <correaluisp@gmail.com>
2017-09-23 10:00:54 -0700

    Criação ajuste

---
+++ djangoecommerce/core/views.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+	texts =['Lorem ipsum dolor sit amet','consectetur adipisicing elit']
+	context = {
+		'title': 'django e-commerce',
+		'texts': texts
+	}
+
+	return render(request, 'index.html', context)
